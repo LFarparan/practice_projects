3580a0e6a01cbd0949b4d7c31436c1abe7e05595
LFarparan <aguinaldo.farparan@gmail.com>
2024-02-25 19:14:04 +0800

    Added startGame & startTally functions

--- Rock-Paper-Scissors/rps_assets/rps_script.js
+++ Rock-Paper-Scissors/rps_assets/rps_script.js
@@ -8,19 +8,45 @@ function getComputerChoice(){
 }
 
 function RPS (playerSelection, computerSelection){
-    let winner =
+    let verdict =
      (playerSelection == computerSelection) ? "You both pick " + playerSelection + ", We have a DRAW!":
-     (playerSelection == 'rock' && computerSelection == 'paper') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Lost!":
+     (playerSelection == 'rock' && computerSelection == 'paper') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Lose!":
      (playerSelection == 'rock' && computerSelection == 'scissors') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Win!!":
      (playerSelection == 'scissors' && computerSelection == 'rock') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Lose!":
      (playerSelection == 'scissors' && computerSelection == 'paper') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Win!!":
      (playerSelection == 'paper' && computerSelection == 'scissors') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Lose!":
      (playerSelection == 'paper' && computerSelection == 'rock') ?  "You pick " +playerSelection+ " against " +computerSelection+ ", You Win!!":
      playerSelection + " is an invalid bet"
-    console.log(winner)
-    return winner
+    let whoWin = (verdict.includes('You Win')) ? "player" : (verdict.includes('You Lose')) ? "computer" : "draw"
+    return {announcement:verdict, winner: whoWin}
 }
 
-let playerSelection = prompt("Choose your bet, Rock, Paper or Scissors");
-let computerSelection = getComputerChoice();
-document.getElementById("result").innerHTML = RPS(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+function startGame (){
+    for (rounds = 1; rounds <= 5; rounds++){
+        let playerSelection = prompt("Choose your bet, Rock, Paper or Scissors");
+        let computerSelection = getComputerChoice();
+        let game = RPS(playerSelection.toLowerCase(), computerSelection.toLowerCase())
+        console.log(game.announcement)
+        if (game.winner == 'player'){
+            playerScore++
+        }
+        else if(game.winner == 'computer'){
+            computerScore++
+        }
+        else console.log('Draw')
+
+        console.log("Player: " +playerScore+ "    Computer: " + computerScore);
+        document.getElementById("result").innerHTML = game.announcement;
+    }
+}
+
+function startTally() {
+    let tally = (playerScore > computerScore) ? "You Beat em AI" : (playerScore < computerScore) ? "AI Supremacy" : "Its a Draw"
+    console.log(tally)
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+startGame()
+startTally()
